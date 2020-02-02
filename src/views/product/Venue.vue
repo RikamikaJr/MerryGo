802197c30177a30d2b5bb11c22ea1031ff9c56dd
RikamikaJr <amirul.oda@gmail.com>
2020-02-02 21:08:07 +0800

    Add Product Detail Page

--- src/views/product/Venue.vue
+++ src/views/product/Venue.vue
@@ -1,392 +1,125 @@
 <template>
-    <v-container class="test">
-		<div class="carousel">
-			<div class="carousel__video" v-for="(video, i) in videos" :class="video.position" :key="(video, i)" >
-				<div @click="changeVideo(0, video.position)" class="inactive" v-if="!video.active">
-					asdasd
-                </div>
-				<img  v-if="!video.active" :src="video.thumbnail"  alt="" class="carousel__video-picture">
-					<div class="carousel__placeholder-background" v-if="video.active">
-
-                    </div>
-			</div>
-		</div>
-    </v-container>
+    <v-app id="inspire">
+        <div class="checklist">
+            <v-card class="overflow-hidden" >
+                <v-app-bar fixed color="black" dark hide-on-scroll>
+                    <v-toolbar-title>MerryGo</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-tooltip bottom>
+                        <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" to="/loginuser">
+                            <v-icon color="grey lighten-1">mdi-nature-people</v-icon>
+                        </v-btn>
+                        </template>
+                        <span>Wedding Planner</span>
+                    </v-tooltip>
+                    <v-tooltip bottom>
+                        <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" to="/loginuser">
+                            <v-icon color="grey lighten-1">mdi-login</v-icon>
+                        </v-btn>
+                        </template>
+                        <span>Login/Signup</span>
+                    </v-tooltip>
+                </v-app-bar>
+            </v-card>
+        </div>
+        <div class="detail">
+            <v-container fluid fill-height>
+                <v-col cols="12" sm="3">
+                    <v-row no-gutters>
+                    <figure class="zoom" v-on:mousemove="zoomProduct" style="background-image: url(https://images.pexels.com/photos/3014853/pexels-photo-3014853.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)">
+                        <img src="https://images.pexels.com/photos/3014853/pexels-photo-3014853.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+                    </figure>
+                    </v-row>
+                   
+                </v-col>
+                <v-col cols="12" sm="9">
+                        <div class="product-summary">
+                        <h2 class="product-title">Basic contrast sneakers</h2>
+                        <div class="user-ratings">
+                            <div class="star-rating">
+                            <span style="width:100%"></span>
+                            </div>
+                        <a href="#reviews" class="total-review" rel="nofollow">(<span class="count"> 5 </span> customer review)</a>
+                        </div>
+                        <div class="price">
+                            <h3>
+                            <span>$</span>
+                            160.00
+                            </h3>
+                            <del>$ 180</del>
+                        </div>
+                        <div class="prodect-details">
+                            <h3>Product Details</h3>
+                            <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem,</p>
+                        </div>
+                        <div class="buy-product">
+                            <div class="stock">
+                            <v-btn color="#4CAF50" dark>IN STOCKS
+                            <v-icon dark right>check_circle</v-icon>
+                            </v-btn>
+                            </div>
+                            <div class="product-qty">
+                            
+                            <div class="cart-btn">
+                            <v-btn  class="px-5" color="error"><i class="fa fa-shopping-cart" aria-hidden="true"></i> ADD TO CART</v-btn>
+                                            <v-btn  class="cart-btn px-5" color="error">BUY NOW</v-btn>
+                            </div>
+                            <button><i class="fa fa-heart" aria-hidden="true"></i>    ADD TO WISHLIST</button>
+                            
+                            </div>
+                        </div>
+                        </div>
+                </v-col>
+            </v-container>
+        </div>
+    </v-app>
 </template>
 
 <script>
-let videoIds = ["sG3BomZqem4", "V0TyHhXkjsE", "nUVRTWWb-8I", "6xz_dbmYc1g", "s3fdI6dw1Tk"];
-    export default {
-        data: function() {
-		return {
-			currentVideo: 0, //refers to tertiary video on leftmost; main video with be currentVideo+2
-			totalVideos: 5,
-			videos: [],
-			videoPositions: {
-				0 : "tertiary tertiary-left",
-				1 : "secondary secondary-left",
-				2 : "main",
-				3 : "secondary secondary-right",
-				4 : "tertiary tertiary-right"
-			}
-		}
-	},
-	computed: {
-		
-	},
-	methods: {
-		changeVideo: function(direction, position) {
-			if(direction == "left") {
-				this.currentVideo--;
-				if(this.currentVideo < 0) {
-					this.currentVideo = 4;
-				}
-				
-			} else if (direction == "right") {
-				this.currentVideo++;
-				if(this.currentVideo > 4) {
-					this.currentVideo = 0;
-				}
-			} else {
-				switch(position) {
-					case 'secondary secondary-right':
-						this.changeVideo('left');
-						break;
-					case 'secondary secondary-left':
-						this.changeVideo('right');
-						break;
-					case 'tertiary tertiary-right':
-						this.changeVideo('left');
-						this.changeVideo('left');
-						break;
-					case 'tertiary tertiary-left':
-						this.changeVideo('right');
-						this.changeVideo('right');
-						break;
-				}
-			}
-			
-			this.videos.forEach((video, i)=>{
-				video.active = false;
-				let newIndex = i+this.currentVideo;
-				if(newIndex > 4) {
-					newIndex %= 5;
-				}
-				video.position = this.videoPositions[newIndex];
-			});
-			
-		}
-	},
-	mounted: function() {
-        var i=''
-		//Generate array of fake carousel videos
-		for (i=0; i<5; i++) {
-			let videoId = videoIds[i];
-			let videoImage = "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg";
-			let video = {
-				thumbnail: videoImage,
-				position: this.videoPositions[i],
-				videoId: videoId,
-				active: false,
-			}
-			
-			this.videos.push(video);
-		}
-	}
+// @ is an alias to /src
+
+export default {
+  
+    data: () => ({
+    }),
+    
+    methods: {
+        zoomProduct: function(e) {
+            var x,offsetY,y,offsetX;
+            var zoomer = e.currentTarget;
+            e.offsetX ? (offsetX = e.offsetX) : (offsetX = e.touches[0].pageX);
+            e.offsetY ? (offsetY = e.offsetY) : (offsetX = e.touches[0].pageX);
+            x = (offsetX / zoomer.offsetWidth) * 100;
+            y = (offsetY / zoomer.offsetHeight) * 100;
+            zoomer.style.backgroundPosition = x + "% " + y + "%";
+            }
+
+
     }
+}
 </script>
 
 <style lang="scss" scoped>
-.test{
-    max-height: 500vh;
-    max-width: 150vh;
-    padding-top: 10vh;
+.detail{
+    margin-top: 8vh;
 }
-.carousel {
-	padding-top: 10vh;
-	flex: 0 1 100%;
-	display: flex;
-	justify-content: center;
-	height: 23rem;
-	position: relative;
-	align-items: center;
-	justify-content: space-evenly;
-    
 
-	&__content {
-		flex: 0 0 80%;
-		height: 100%;
-		position: relative;
-        
-	}
-
-	&__overlay {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		width: 90%;
-		height: 90%;
-		border-radius: 5px;
-		color: white;
-		z-index: 2;
-		display: flex;
-		flex-wrap: wrap;
-		align-content: space-between;
-        
-
-        
-		
-	}
-
-	&__video {
-		width: 550px;
-		height: 310px;
-		position: absolute;
-		transition: all 0.5s;
-
-        
-		
-		& .inactive {
-			position: absolute;
-			z-index: 1;
-			top: 0;
-			left: 0;
-			height: 100%;
-			width: 100%;
-			background-color: rgba(0, 0, 0, 0.5);
-			border-radius: 5px;
-			transition: all .2s ease-out;
-			
-			
-			&:hover {
-				cursor: pointer;
-				transform: scale(1.02);
-				background-color: rgba(0, 0, 0, 0.4);
-			}
-			
-			&:hover + img {
-				transform: scale(1.02);
-			}
-		}
-
-		&-background {
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
-			background-color: rgba(0, 0, 0, 0.5);
-			z-index: 3;
-		}
-
-
-		&.main {
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			z-index: 3;
-			
-			& .inactive {
-				
-				& + img {
-					transition: none;
-				}
-
-				&:hover {
-					cursor: pointer;
-					transform: scale(1);
-				background-color: rgba(0, 0, 0, 0.1);
-				}
-
-				&:hover + img {
-					transform: scale(1);
-				}
-			}
-			
-		}
-
-
-		&.secondary {
-			width: 467.5px;
-			height: 263.5px;
-			z-index: 2;
-			&-left {
-				top: 50%;
-				left: 20%;
-				transform: translate(-20%, -50%);
-			}
-			&-right {
-				top: 50%;
-				left: 80%;
-				transform: translate(-80%, -50%);
-			}
-		}
-
-		&.tertiary {
-			width: 385px;
-			height: 217px;
-			z-index: 1;
-			&-left {
-				top: 50%;
-				left: 0;
-				transform: translate(0, -50%);
-			}
-			&-right {
-				top: 50%;
-				left: 100%;
-				transform: translate(-100%, -50%);
-			}
-		}
-
-		&-picture {
-			width: 100%;
-			height: 100%;
-			border-radius: 5px;
-            
-		}
-	}
+figure.zoom {
+  & img:hover {
+    opacity: 0;
+  }
+  img {
+    transition: opacity .5s;
+    display: block;
+    width: 100%;
+    max-height: 650px;
+  }
+  background-position: 50% 50%;
+  position: relative;
+  width: 500px;
+  overflow: hidden;
+  cursor: zoom-in;
 }
-@media screen and (max-width: 759px){
-.test{
-    max-height: 500vh;
-    max-width: 300px;
-}
-.carousel {
-	margin-top: -12vh;
-	flex: 0 1 100%;
-	display: flex;
-	justify-content: center;
-	height: 23rem;
-	position: relative;
-	align-items: center;
-	justify-content: space-evenly;
-
-	&__content {
-		flex: 0 0 80%;
-		height: 100%;
-		position: relative;
-	}
-
-	&__overlay {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		width: 90%;
-		height: 90%;
-		border-radius: 5px;
-		color: white;
-		z-index: 2;
-		display: flex;
-		flex-wrap: wrap;
-		align-content: space-between;
-		
-	}
-
-	&__video {
-		width: 205px;
-		height: 155px;
-		position: absolute;
-		transition: all 0.5s;
-		
-		& .inactive {
-			position: absolute;
-			z-index: 1;
-			top: 0;
-			left: 0;
-			height: 100%;
-			width: 100%;
-			background-color: rgba(0, 0, 0, 0.6);
-			border-radius: 5px;
-			transition: all .2s ease-out;
-			
-			
-			&:hover {
-				cursor: pointer;
-				transform: scale(1.02);
-				background-color: rgba(0, 0, 0, 0.4);
-			}
-			
-			&:hover + img {
-				transform: scale(1.02);
-			}
-		}
-
-		&-background {
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
-			background-color: rgba(0, 0, 0, 1);
-			z-index: 3;
-		}
-
-
-		&.main {
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			z-index: 3;
-			
-			& .inactive {
-				
-				& + img {
-					transition: none;
-				}
-
-				&:hover {
-					cursor: initial;
-					transform: scale(1);
-				background-color: rgba(0, 0, 0, 0.6);
-				}
-
-				&:hover + img {
-					transform: scale(1);
-				}
-			}
-			
-		}
-
-
-		&.secondary {
-			width: 230px;
-			height: 130px;
-			z-index: 2;
-			&-left {
-				top: 50%;
-				left: 20%;
-				transform: translate(-20%, -50%);
-			}
-			&-right {
-				top: 50%;
-				left: 80%;
-				transform: translate(-80%, -50%);
-			}
-		}
-
-		&.tertiary {
-			width: 200px;
-			height: 105px;
-			z-index: 1;
-			&-left {
-				top: 50%;
-				left: 0;
-				transform: translate(0, -50%);
-			}
-			&-right {
-				top: 50%;
-				left: 100%;
-				transform: translate(-100%, -50%);
-			}
-		}
-
-		&-picture {
-			width: 100%;
-			height: 100%;
-			border-radius: 5px;
-		}
-	}
-}}
 </style>
